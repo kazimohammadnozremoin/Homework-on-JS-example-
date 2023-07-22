ff85d5f358b3f452072f9cc5beb6a0d9f11e25e1
kazi Mohammad Nozre Moin <kazimoyeen@gmail.com>
2023-07-22 22:03:45 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+//text-change
+function textChange(){
+  document.getElementById('h2').innerHTML="Hello there!";
+}
+//text show/hide
+function textShow(){
+  document.getElementById('tsh').style.display="block";
+}
+
+function textHide(){
+  document.getElementById('tsh').style.display="none";
+}
+//show date
+function showDate(){
+  document.getElementById('tsd').innerHTML="Date()";
+}
+//bulb on/off
+function bulbOn(){
+  document.getElementById('bulb').src="images/bulbon.gif";
+}
+
+function bulbOff(){
+  document.getElementById('bulb').src="images/bulboff2.gif";
+}
+
